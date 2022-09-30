cc992f7fbf4a74bdbd1ad9bafc22bcc2cb7a1ff7
fodo <goodbumsu@gmail.com>
2022-10-01 01:42:31 +0900

    add signer dup check
    Signed-off-by: fodo <goodbumsu@gmail.com>

--- cmd/osmosisd/cmd/indexrange.go
+++ cmd/osmosisd/cmd/indexrange.go
@@ -16,11 +16,11 @@ import (
 	"os/exec"
 	"strconv"
 
-	"sync"
-	"runtime"
 	"github.com/cosmos/cosmos-sdk/client"
 	"github.com/gogo/protobuf/jsonpb"
 	"github.com/tendermint/tendermint/config"
+	"runtime"
+	"sync"
 )
 
 const (
@@ -88,16 +88,18 @@ func indexRange() *cobra.Command {
 				db_bs.Close()
 				db_ss.Close()
 			}()
-		
+
 			bs := tmstore.NewBlockStore(db_bs)
 			ss := tmstate.NewStore(db_ss)
 
 			cpus := int64(runtime.NumCPU())
-			for i:=int64(0);i<int64(cpus);i++ {
+			for i := int64(0); i < int64(cpus); i++ {
 				wg.Add(1)
-				window := (endHeight - startHeight)/cpus
-				end := startHeight+window*(i+1)-1
-				if i == cpus-1 { end = endHeight }
+				window := (endHeight - startHeight) / cpus
+				end := startHeight + window*(i+1) - 1
+				if i == cpus-1 {
+					end = endHeight
+				}
 				go func(gdbPath string, startHeight int64, endHeight int64, num int64, gbs *tmstore.BlockStore, gss *tmstate.Store) {
 					fmt.Printf("%d %d\n", startHeight, endHeight)
 					err = indexRangeOfBlocks(dbPath, startHeight, endHeight, num, gbs, gss /*, irConnStrFlag, numThread, irIndexEventFlag == "true", irDescFlag == "true"*/)
@@ -108,7 +110,7 @@ func indexRange() *cobra.Command {
 					defer wg.Done()
 				}(dbPath, startHeight+window*i, end, i, bs, &ss)
 			}
-			wg.Wait()	
+			wg.Wait()
 
 			fmt.Println("Done ...")
 
@@ -124,7 +126,6 @@ func indexRange() *cobra.Command {
 }
 
 func indexRangeOfBlocks(dbPath string, startHeight int64, endHeight int64, num int64, bs *tmstore.BlockStore, ss *tmstate.Store /*, connStr string, numThreads int64, eventIndex bool, desc bool*/) error {
-	
 
 	config := app.MakeEncodingConfig()
 
@@ -146,8 +147,6 @@ func indexRangeOfBlocks(dbPath string, startHeight int64, endHeight int64, num i
 		}
 	}
 
-
-	
 	jsonpbMarshaller := jsonpb.Marshaler{}
 
 	defer func() {
@@ -184,7 +183,12 @@ func indexRangeOfBlocks(dbPath string, startHeight int64, endHeight int64, num i
 			txResultFile.WriteString(fmt.Sprintf("%d\t%d\t'%s'\t'%s'\t'%s'\t%d\t'%s'\t'%d'\t'%d'\t'%s'\n", block.Height, j, timestamp, txHash, resultString, code, codespace, gasUsed, gasWanted, info))
 			//flush to tx_results
 			for k, m := range cosmosTx.GetMsgs() {
+				sMap := make(map[string]string)
 				for _, s := range m.GetSigners() {
+					if _, ok := sMap[s.String()]; ok {
+						continue
+					}
+					sMap[s.String()] = "exist"
 					msgString, err := jsonpbMarshaller.MarshalToString(m)
 					if err != nil {
 						return err
